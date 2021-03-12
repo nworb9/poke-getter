73b6a63c0f2c745b3a5a1ee64d541b4cf83dfaab
nworb95 <nworb95@gmail.com>
2021-03-12 17:24:16 -0500

    Testing endpoint

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def howdy_world():
     return 'Howdy world!'
+
+
+@app.route('/pokemon/catch', methods=['GET'])
+def catch_pokemon():
+    if request.args.get('type'):
+        pokemon_type = request.args.get('type')
+    if request.args.get('habitat_name'):
+        pokemon_habitat = request.args.get('habitat_name')
+    if request.args.get('habitat_id'):
+        pokemon_habitat = request.args.get('habitat_id')
+    return pokemon_type
